3d024bc39eac19ff5d0e73a2bb438cf108c0c0b5
Cheolkie Kim <ckiekim@naver.com>
2023-06-02 10:29:15 +0900

    이미지 생성

--- 99.SampleProject/bp_chatbot/chatbot.py
+++ 99.SampleProject/bp_chatbot/chatbot.py
@@ -47,3 +47,23 @@ def bard():
         req = requests.post('https://api.bardapi.dev/chat', headers=headers, json=data)
         result = { 'user':user_input, 'chatbot':req.json()['output'] }
         return json.dumps(result)
+
+@chatbot_bp.route('/genImg', methods=['GET','POST']) 
+def gen_img():
+    if request.method == 'GET':
+        return render_template('chatbot/chatGPT.html', menu=menu)
+    else:
+        with open(os.path.join(current_app.static_folder, 'keys/openAIapikey.txt')) as f:
+            openAI_key = f.read()
+        user_input = request.form['userInput']  # 'a photo of a happy corgi puppy sitting and facing me'
+        headers = { 'Authorization': f'Bearer {openAI_key}', 
+                    'Content-Type': 'application/json' }
+        data = { 'model': 'image-alpha-001',
+                 'prompt': quote(user_input),
+                 'num_images': 1 }
+        req = requests.post('https://api.openai.com/v1/images/generations', 
+                            headers=headers, data=json.dumps(data))
+
+        result = req.json()
+        print(result)
+        return json.dumps(result)
